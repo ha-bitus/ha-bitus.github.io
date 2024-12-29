4e30df3c6c949d691ede07de1910f1155ec994c6
emilymcaleese10 <103384151+emilymcaleese10@users.noreply.github.com>
2024-12-29 18:45:20 +0000

    Updated form url

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ document.getElementById('submissionForm').addEventListener('submit', function(e)
 
     var email_list = document.getElementById('email').value;
 
-    var url_list = 'https://docs.google.com/forms/d/e/1FAIpQLSd6hvdmGvy7Xd5281G2PXnyXKWSEX9rLzK5X8P7fqL2P4H0qw/viewform';
+    var url_list = 'https://docs.google.com/forms/d/e/1FAIpQLSd6hvdmGvy7Xd5281G2PXnyXKWSEX9rLzK5X8P7fqL2P4H0qw/formResponse';
     var formData = new FormData();
     formData.append('entry.772505350', email_list);
 
     fetch(url_list, {
         method: 'POST',
         body: formData,
-        mode: 'no-cors'
+        // mode: 'no-cors'
     }).then(response => {
         alert('Form submitted successfully!');
     }).catch(error => {
